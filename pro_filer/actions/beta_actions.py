4ff92a1891a49d357e6376f4deee3ca20d4aac6c
Felipe Duarte <feduarte.dev@gmail.com>
2024-03-28 17:05:52 -0300

    requisito 2

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -18,11 +18,28 @@ def find_file_by_name(context, search_term, case_sensitive=True):
     for path in context["all_files"]:
         file_name = path.split("/")[-1]
 
-        if not case_sensitive:
-            file_name.lower()
-            search_term.lower()
+        new_file_name = file_name
+        new_search_term = search_term
 
-        if search_term in file_name:
+        if not case_sensitive:
+            new_file_name = file_name.lower()
+            new_search_term = search_term.lower()
+
+        if new_search_term in new_file_name:
             found_files.append(path)
 
     return found_files
+
+
+teste = {
+    "all_files": [
+        "/path/to/file.sql",
+        "/path/to/file.txt",
+        "/path/to/file2.txt",
+        "/path/to/FILE.txt",
+        "/path/to/FILE2.TXT",
+        "/path-to/file.txt",
+    ]
+}
+
+print(find_file_by_name(teste, "FILE", False))
