16c86e1cb2c514008da4c6e0a7a4073b26e0a14e
Felipe Duarte <feduarte.dev@gmail.com>
2024-04-01 17:23:00 -0300

    requisito 5 refatorado

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -3,27 +3,18 @@ from pro_filer.actions.main_actions import show_disk_usage  # NOQA
 from unittest.mock import Mock
 
 
-# nao consegui usar o mesmo padrao de show details, onde faço a fixture usando tmp_path
-def test_show_disk_usage_success(monkeypatch, tmp_path, capsys):
-    output_path = tmp_path / "test.py"
-    output_path.write_text("test 1")
-    output_path = str(output_path)
-
-    output_path_2 = tmp_path / "test2.py"
-    output_path_2.write_text("aaaaaaaaaaaaaaa")
-    output_path_2 = str(output_path_2)
-
+def test_show_disk_usage_success(monkeypatch, capsys, setup):
     mock_function = "pro_filer.actions.main_actions._get_printable_file_path"
     mock_result = Mock(return_value="test.py")
     monkeypatch.setattr(mock_function, mock_result)
 
-    context = {"all_files": [output_path, output_path_2]}
+    no_extension, with_extension_2 = setup
+    context = {"all_files": [no_extension, with_extension_2]}
     show_disk_usage(context)
 
     captured = capsys.readouterr()
-    # pq eu precisei diminuir a distancia dos textos? na funcao ta ljust 70
-    expected_output = f"""'test.py':{''.ljust(60)} 15 (71%)
-'test.py':{''.ljust(60)} 6 (28%)
-Total size: 21\n"""
+    expected_output = f"""'test.py':{''.ljust(60)} 17 (54%)
+'test.py':{''.ljust(60)} 14 (45%)
+Total size: 31\n"""
 
     assert captured.out == expected_output
